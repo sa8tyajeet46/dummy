2c0fabff8b5fc3118a6d3ad39bdde34092015504
sa8tyajeet46 <satyajeettripathy677@gmail.com>
2023-10-21 16:22:30 +0530

    kl

--- pages/api/connectDatabase.js
+++ pages/api/connectDatabase.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectModule: require("pg"),
+});
 module.exports = async () => {
   try {
     await sequelize.authenticate();
